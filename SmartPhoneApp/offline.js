d736d34903b1aa0447a1dd303db6028330c5059a
RUBO Marius <marius.rubo@unifr.ch>
2019-09-02 16:27:58 +0200

    update sp

---
+++ SmartPhoneApp/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1567434449,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/camera-sheet0.png",
+		"images/fribourg_logo-sheet0.png",
+		"images/clouds-sheet0.png",
+		"images/background-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
